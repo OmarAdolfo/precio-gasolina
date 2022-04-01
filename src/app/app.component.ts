cea5d7e777a84253cef3bfa53fc1ee45584c5fc7
oalvher <oalvher95@gmail.com>
2022-04-01 17:09:51 +0100

    Se añade pwa Se crea formulario Se añaden validaciones Se hace función del cálculo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'precio-gasolina';
+export class AppComponent implements OnInit {
+
+  calculadoraForm: FormGroup;
+  resultado: string;
+
+  ngOnInit(): void {
+    this.calculadoraForm = new FormGroup({
+      dineroapagar: new FormControl('', Validators.required),
+      preciogasolina: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+([.][0-9][0-9]?)?$')]),
+    });
+  }
+
+  calcularprecio() {
+    const dineroapagar = +this.calculadoraForm.get('dineroapagar').value;
+    const preciogasolina = +this.calculadoraForm.get('preciogasolina').value;
+    console.log(dineroapagar);
+    console.log(preciogasolina);
+    let litros = dineroapagar / preciogasolina;
+    let descuento = litros * 0.2;
+    let preciopagado = dineroapagar - descuento;
+    this.resultado = ((dineroapagar * dineroapagar) / preciopagado).toFixed(2);
+  }
 }
